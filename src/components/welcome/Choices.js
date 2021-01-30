0809410649516d3fdfdc76d5d474a140b6b35096
Karine325 <61804667+Karine325@users.noreply.github.com>
2021-01-30 08:02:47 -0500

    updated file

--- src/components/welcome/Choices.js
+++ src/components/welcome/Choices.js
@@ -1,19 +1,97 @@
 import React from 'react';
-import logo from '../../images/logo.png';
 import doggo from '../../images/doggo.png';
 import treat from '../../images/treat.png';
 import vet from '../../images/vet.png';
 
+{/*
+const { useState } = React;
+
+const Product = ({ product, children }) => (
+  <div className="products">
+    {product.name}
+    {children}
+  </div>
+);
 export default function Choices() {
-    return (
-        <h1>Hello</h1>
-        /*<div className="header-container" style={{margin: "0px"}}>
-            <div class="d-flex justify-content-center">
-            <div className="logo">
-                <img src={logo} al="logo"/>
-            </div>
-         </div>
+    const [products] = useState([
+        { name: "PETS", logo: doggo },
+        { name: "VETS", logo: vet },
+        { name: "STORE", logo: treat }
+      ]);
+
+      return (
+        <div className="App">
+          <div className="productsContainer">
+            {products.map((product, index) => (
+              <Product key={index} product={product}>           
+                <img src={product.logo} alt="website logo" />
+                
+                <button>Add to cart</button>
+              </Product>
+            ))}
+          </div>
         </div>
-        */
-    )
+      );
+}
+*/}
+
+export default function Choices(){
+    
+    return(
+      <>
+      <div class="d-flex justify-content-around pt-5" >
+        <div className="Dog">
+            <img src={doggo} alt="Doggo" />
+            <div class="d-flex justify-content-around mt-1" >
+            </div>
+        </div>
+
+        <div className="Vet">
+            <img src={vet} alt="Vet" />
+            <div class="d-flex justify-content-around mt-1" >
+            </div>
+        </div>
+
+        <div className="Treat">
+            <img src={treat} alt="Treat" />
+            <div class="d-flex justify-content-around mt-1" >
+            </div>
+        </div>
+        
+      </div>
+
+      <div class="d-flex justify-content-around" >
+        <p class="fw-bold">
+          PETS
+        </p>
+
+        <p class="fw-bold">
+          VETS
+        </p>
+
+        <p class="fw-bold">
+          STORE
+        </p>
+      </div>
+
+      <div class="d-flex justify-content-around p-3 mt-4">
+        <p class="fw-bolder">Make A New Friend</p>
+
+        <p class="fw-bolder">Find Nearby Clinics</p>
+
+        <p class="fw-bolder">Treat Your Pet</p>
+      </div>
+
+      <div class="d-flex justify-content-around">
+      <button className="btn btn-outline-dark"><a href="/">Adopt Now</a>
+      </button>
+
+      <button className="btn btn-outline-dark"><a href="/">View Now</a>
+      </button>
+
+      <button className="btn btn-outline-dark"><a href="/">Shop Now</a>
+      </button>
+      </div>
+      </>
+    );
 }
--- src/components/welcome/LandingPage.js
+++ src/components/welcome/LandingPage.js
@@ -16,33 +16,20 @@ export default function LandingPage() {
         <div className="choices-container">
             <Choices/>
         </div>
-            {/*}
-            <div class="post-container">
-                <div className="post-thumb"><img src={cat} alt="cat" class="float-right"/></div>
-                <div class="post-content">
-                    <h3 className="post-title"> Make a New Friend.</h3>
-                    <button className="adopt-btn"> Adopt Now!</button>
-                </div>
-            </div>
-            */}
 
 
         {/*<div className="divider"></div>*/}
-        <div className="spacing"> helloo</div>
+        {/*<div className="spacing"> helloo</div>*/}
        
        <div className="utilities">
             <div class="d-flex justify-content-center">
-                     <h3 class="font-weight-bold"> Trusty Paws</h3>      
+                     <h3 class="fs-6 fw-bold"> Trusty Paws</h3>      
                 
-              
             </div> 
-            <p class="text-center">From finding local friends, to  your  
-                    routinely morning walks , . Trusty Paws has  thought of it </p>
-            <p class="text-center">and made it better. Join the community , make a new friend, connect with local 
+            <p class="text-center pe-5 ps-5">From finding local friends, to  your  
+                    routinely morning walks, Trusty Paws has  thought of it and made it better. Join the community , make a new friend, connect with local 
                     & qualified vetenerians and shelters and individuals who sell quality essientials and treats.</p>
-            <div className="paragraph">
-                 <h5></h5>
-            </div> 
+
         </div>
         </>
     )

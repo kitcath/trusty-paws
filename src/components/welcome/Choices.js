80e778156440fcb1858697cbf7cf741827efa3a2
Karine325 <61804667+Karine325@users.noreply.github.com>
2021-01-30 03:02:12 -0500

    created file

---
+++ src/components/welcome/Choices.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import doggo from '../../images/doggo.png';
+import treat from '../../images/treat.png';
+import vet from '../../images/vet.png';
+
+export default function Choices() {
+    return (
+        <h1>Hello</h1>
+        /*<div className="header-container" style={{margin: "0px"}}>
+            <div class="d-flex justify-content-center">
+            <div className="logo">
+                <img src={logo} al="logo"/>
+            </div>
+         </div>
+        </div>
+        */
+    )
+}
